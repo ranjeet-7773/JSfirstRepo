92b8ad0a77c00f84b2072a5f018a45345da9130e
ranjeet-7773 <ranjeetsinghsisodiya2718@gmail.com>
2024-04-24 18:47:28 +0530

    added

--- first.js
+++ first.js
@@ -7,11 +7,30 @@
      bio:"To educate someone is highest privilage",
 
 }; */
-let num = prompt("Enter a number");
 
-if(num%5 ===0){
-    console.log(num ,"is multiple of 5");
+
+ /*let numm = prompt("Enter a number");
+
+if(numm%5 ===0){
+    console.log(numm ,"is multiple of 5");
 }
 else{
-    console.log(num,"not a multiuple of 5");
+    console.log(numm,"not a multiuple of 5");
+} */
+
+
+
+/*let gameNum=25;
+let num = prompt("enter a number");
+let count=0; 
+while(num != gameNum){
+   num = prompt("you have entered wrong number. Guess Again !");
+   count++
+
 }
+console.log("total attempt = " , count);
+console.log("congratulations You win"); */
+
+let userName=prompt("enter your username");
+let output= "@"+userName+userName.length;
+console.log(output);
