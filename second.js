70e6497c2716be85fdc665e6c73c38dc1ef4b14a
ranjeet-7773 <ranjeetsinghsisodiya2718@gmail.com>
2024-04-25 13:34:08 +0530

    new file

---
+++ second.js
@@ -0,0 +1,7 @@
+let arr=["Bloomberg","Microsoft","Google","Uber","IBM","Netflix"];
+arr.shift();
+console.log(`companies are : ${arr}`);
+ arr.splice(2,1,"Ola"); 
+ console.log(`companies are : ${arr}`);
+ arr.push("Amazon");
+ console.log(`companies are : ${arr}`);
