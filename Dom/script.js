2ee3fd2b2a377d89dc794075eb4d8e1323781a04
ranjeet-7773 <ranjeetsinghsisodiya2718@gmail.com>
2024-04-26 23:24:12 +0530

    new files

--- Dom/script.js
+++ Dom/script.js
@@ -1,5 +1,32 @@
-let div = document.querySelector(".Box");
-div.onmouseover = () => {
+
+//Events
+//let btn1 = document.querySelector(".Box");
+/* div.onmouseover = () => {
     console.log("you are inside div");
+
+} */ 
+//EventListner
+
+/* btn1.addEventListener("click" , ()=>{
+    console.log("hover done");
+} ); */
+
+//ToggleButton
+
+let modeBtn = document.querySelector("#mode");
+let currentMode = "Light";
+modeBtn.addEventListener("click" , ()=>{
     
-}
+    if(currentMode==="Light"){
+        currentMode="dark";
+        
+        document.querySelector("body").style.backgroundColor="black";
+        let h2=document.querySelector("dark");
+        console.log("This is dark mode");
+    }
+    else{
+        currentMode="Light";
+        document.querySelector("body").style.backgroundColor="white";
+    }
+    console.log(currentMode);
+} );
